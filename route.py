f30f1331f6e318c0a67b10ef4854080e400c341b
skinan <sakib.khaninan@gmail.com>
2020-09-07 14:29:49 +0600

    gpa history working fine with database

--- route.py
+++ route.py
@@ -25,17 +25,18 @@ def calculate():
 
     # validate form data (what happens when form data not correctly fetched)
     if formData:
-
-        grades_credits_list, grade_sheet = DataPreProcessingGPA(formData).clean_data()
-        gpa = calculate_gpa.calculate(grades_credits_list, grade_sheet)
-        scale_value_list = list(grade_sheet.values())
-        scale = max(scale_value_list)
-        user_gpa_history = GPA_HISTORY(scale=float(scale), gpa=float(gpa), grade_sheet=str(grade_sheet))
-        db.session.add(user_gpa_history)
-        db.session.commit()
-        return jsonify(
-                {"GPA": gpa}
-            )
-
+        try:
+            grades_credits_list, grade_sheet = DataPreProcessingGPA(formData).clean_data()
+            gpa = calculate_gpa.calculate(grades_credits_list, grade_sheet)
+            scale_value_list = list(grade_sheet.values())
+            scale = max(scale_value_list)
+            user_gpa_history = GPA_HISTORY(scale=float(scale), gpa=float(gpa), grade_sheet=str(grade_sheet))
+            db.session.add(user_gpa_history)
+            db.session.commit()
+            return jsonify(
+                {"GPA": gpa})
+        except:
+            return jsonify(
+                {"GPA": -1})
     else:
         return jsonify({"GPA": -1})
